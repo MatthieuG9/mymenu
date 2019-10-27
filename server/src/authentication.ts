6e3ed29a6a350c1563dc1fc36765f8ba774d5636
Matthieu <matthieu.giraud99@gmail.com>
2019-10-27 16:43:20 +0100

    Authentfication via Google ok

--- server/src/authentication.ts
+++ server/src/authentication.ts
@@ -1,7 +1,7 @@
-import { ServiceAddons } from '@feathersjs/feathers';
+import { ServiceAddons, Params } from '@feathersjs/feathers';
 import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
 import { LocalStrategy } from '@feathersjs/authentication-local';
-import { expressOauth } from '@feathersjs/authentication-oauth';
+import { expressOauth, OAuthStrategy, OAuthProfile } from '@feathersjs/authentication-oauth';
 
 import { Application } from './declarations';
 
@@ -11,11 +11,27 @@ declare module './declarations' {
   }
 }
 
+class GoogleStrategy extends OAuthStrategy {
+  async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
+  
+    // this will set 'googleId'
+    const baseData = await super.getEntityData(profile, existing, params);
+    
+    // this will grab the picture and email address of the Google profile
+    return {
+      ...baseData,
+      profilePicture: profile.picture,
+      email: profile.email
+    };
+  }
+}
+
 export default function(app: Application) {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
+  authentication.register('google', new GoogleStrategy());
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
